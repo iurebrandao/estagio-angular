8554e3db4b20afe7371b00742d047c11adbf0c5b
iurebrandao <iurebrandao@gmail.com>
2017-07-31 19:25:01 -0300

    Documentação (Sistema Finalizado)

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -7,38 +7,62 @@ angular.module('myApp.view1', ['ngRoute'])
     .controller('View1Ctrl', ["$scope","$http", "config", function ($scope, $http, config) {
         var vm = this;
 
+        // Inicializa a variável payments para receber os pagamentos de cada cartão
         $scope.payments={};
 
+        // Função que recupera a lista de cartões existentes e mostra na tabela de cartões na view
         vm.getCards = function () {
 
+            // Cria o elemento "spinner" que indica o carregamento das informações para o usuário
             document.getElementById("loader").style.display = "block";
 
+            // Faz a requisição "Get" para receber a lista de cartões
             $http({
                 method: "GET",
                 headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + config.KEY
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + config.KEY
                 },
                 url: config.URL + "cards"
             }).then(function (response) {
+
+                // Esconde o elemento "spinner" que indica o carregamento das informações para o usuário
                 document.getElementById("loader").style.display = "none";
+
+                // Adiciona as informações de retorno (lista de cartões) da requisição
+                // na variável do controler
                 vm.data = response.data;
 
                 if(vm.data.length < 1){
+                    // Mensagem adicionada para aparecer na view como retorno para o usuário
                     $scope.msg_user = "Não há nenhum cartão registrado em nosso banco de dados!";
                 }
 
             }, function (response) {
+
+                // Esconde o elemento "spinner" que indica o carregamento das informações para o usuário
                 document.getElementById("loader").style.display = "none";
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data || 'Request failed';
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao carregar a lista de cartões";
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
+                vm.getCards();
 
             });
         };
 
+        // Função que altera as informações de um cartão a partir das informações novas que o
+        // usuário quer editar, que estão no "array_info"
         vm.editCard = function (array_info) {
+
             var mes = parseInt(array_info[4]);
             var ano = parseInt(array_info[5]);
+
+            // Faz a requisição "PATCH" para editar um cartão
             $http({
                 method: "PATCH",
                 url: config.URL + "cards/" + array_info[0],
@@ -53,23 +77,38 @@ angular.module('myApp.view1', ['ngRoute'])
                     "brand": array_info[3],
                     "exp_month": mes,
                     "exp_year": ano,
-                    "limit": array_info[6],
+                    "limit": array_info[6]
                 }
             }).then(function (response) {
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data;
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user = "Cartão editado com sucesso!";
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
 
 
             }, function (response)  {
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data || 'Request failed';
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao editar o cartão";
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
 
             });
         };
 
+        // Função que deleta um cartão da lista de cartões a partir do "id" desse cartão
         vm.deleteCard = function (card_id) {
+
+            // Faz a requisição "DELETE" para deletar um cartão
             $http({
                 method: "DELETE",
                 url: config.URL + "cards/" + card_id,
@@ -81,18 +120,28 @@ angular.module('myApp.view1', ['ngRoute'])
                     "id": card_id
                 }
             }).then(function (response) {
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user = "Cartão removido com sucesso!";
 
             }, function (response) {
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data || 'Request failed';
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao deletar o cartão";
 
             });
         };
 
+        // Funcão que recupera os pagamentos de um determinado cartão a partir do "id" dele
         $scope.getPayments = function (card_id) {
 
+            // Cria o elemento "spinner" que indica o carregamento das informações para o usuário
             var elements = document.getElementsByClassName("loader-pay");
             for(var i=0;i<elements.length;i++){
                 if(elements[i].id === "loader"+card_id){
@@ -100,6 +149,7 @@ angular.module('myApp.view1', ['ngRoute'])
                 }
             }
 
+            // Faz a requisição "GET" para receber os pagamentos de um determinado cartão
             $http({
                 method: "GET",
                 headers: {
@@ -109,6 +159,7 @@ angular.module('myApp.view1', ['ngRoute'])
                 url: config.URL + "cards/" + card_id + "/payments"
             }).then(function (response) {
 
+                // Esconde o elemento "spinner" que indica o carregamento das informações para o usuário
                 var elements = document.getElementsByClassName("loader-pay");
                 for(var i=0;i<elements.length;i++){
                     if(elements[i].id === "loader"+card_id){
@@ -116,18 +167,25 @@ angular.module('myApp.view1', ['ngRoute'])
                     }
                 }
 
+                // Adiciona as informações de pagamento em "payments"
+                // para mostrar na tabela de pagamentos na view
                 $scope.payments[0] = response.data;
 
                 if(response.data.length < 1){
+                    // Mensagem adicionada para aparecer na view como retorno para o usuário
                     $scope.msg_user_pay = "Não há nenhum pagamento registrado neste cartão";
                 }
                 else{
+                    // Limpa a mensagem aparecida para o usuário
                     $scope.msg_user_pay = null;
                 }
 
             }, function (response) {
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error_pay = "Erro ao carregar os pagamentos desse cartão";
 
+                // Esconde o elemento "spinner" que indica o carregamento das informações para o usuário
                 var elements = document.getElementsByClassName("loader-pay");
                 for(var i=0;i<elements.length;i++){
                     if(elements[i].id === "loader"+card_id){
@@ -138,7 +196,11 @@ angular.module('myApp.view1', ['ngRoute'])
             });
         };
 
+        // Função que faz o pagamento de um cartão a partir do "id" do cartão e o valor do pagamento
+        // contidos na variável "array_info"
         vm.makePayment = function (array_info) {
+
+            // Faz a requisição "POST" para fazer o pagamento de um cartão
             $http({
                 method: "POST",
                 url: config.URL + "payments",
@@ -148,20 +210,34 @@ angular.module('myApp.view1', ['ngRoute'])
                 },
                 data: {
                     "card_id": array_info[0],
-                    "amount": array_info[1],
+                    "amount": array_info[1]
                 }
+
             }).then(function (response) {
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data;
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user = "Pagamento feito com sucesso!";
+
+                // Apaga o valor do input "valor"
                 $scope.valor = null;
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
 
             }, function (response) {
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao fazer o pagamento! Valor do pagamento maior que o disponível";
             });
         };
 
+        // Função que deleta um pagamento a partir do "id" dele
         vm.deletePayment = function (pay_id) {
+
+            // Faz a requisição "DELETE" para deletar um pagamento
             $http({
                 method: "DELETE",
                 url: config.URL + "payments/" + pay_id,
@@ -172,25 +248,40 @@ angular.module('myApp.view1', ['ngRoute'])
                 data: {
                     "id": pay_id
                 }
+
             }).then(function (response) {
+
+                // Adiciona as informações de retorno da requisição na variável do controler
                 vm.data = response.data;
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user = "Pagamento removido com sucesso!";
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
 
 
             }, function (response) {
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao deletar o pagamento";
 
             });
         };
 
+        // Função que edita o status de um pagamento a partir do "id". Essas duas informações
+        // são recebidas pelo "array_info"
         vm.editPayment = function (array_info) {
 
             var status = '';
 
+            // A partir de um item selecionado pelo usuário, é colocado o status correto para fazer
+            // a requisição
             if(array_info[1] === 'Pago'){
-                 status = 'paid';
+                status = 'paid';
             }
             else if(array_info[1] === 'Pendente'){
                 status = 'pending';
@@ -199,6 +290,7 @@ angular.module('myApp.view1', ['ngRoute'])
                 status = 'failed';
             }
 
+            // Faz a requisição "PATCH" para editar o status do pagamento
             $http({
                 method: "PATCH",
                 url: config.URL + "payments/" + array_info[0],
@@ -211,11 +303,17 @@ angular.module('myApp.view1', ['ngRoute'])
                     "status": status
                 }
             }).then(function (response) {
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user = "Pagamento editado com sucesso!";
+
+                // Chama a função para atualizar a lista de cartões com as novas informações
                 vm.getCards();
 
 
             }, function (response)  {
+
+                // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao editar o pagamento";
 
             });
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -4,8 +4,9 @@ angular.module('myApp.view2', ['ngRoute'])
 
     .controller('View2Ctrl', ["$scope", "$location", "$http", "config",
         function ($scope, $location, $http, config) {
-        var vm = this;
+            var vm = this;
 
+            // Função para resetar os campos do formulário de criacão de um novo cartão
             $scope.resetForm = function() {
                 $scope.nome = null;
                 $scope.numero = null;
@@ -15,31 +16,39 @@ angular.module('myApp.view2', ['ngRoute'])
                 $scope.limite = null;
             };
 
+            // Função que cria um cartão a partir das informações inseridas pelo usuário.
+            // Essas informações são armazenadas no "array_info".
+            $scope.createCard = function (array_info) {
 
-        $scope.createCard = function (array_info) {
-            $http({
-                method: "POST",
-                url: config.URL + "cards",
-                headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + config.KEY
-                },
-                data: {
+                // Faz a requisição "POST" para criar um novo cartão
+                $http({
+                    method: "POST",
+                    url: config.URL + "cards",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + config.KEY
+                    },
+                    data: {
                         "name": array_info[0],
                         "number": array_info[1],
                         "brand": array_info[2],
                         "exp_month":array_info[3],
                         "exp_year": array_info[4],
                         "limit": array_info[5],
-                }
-            }).then(function (response) {
-                vm.data = response.data;
-                $scope.msg = "Cartão adicionado com sucesso!";
-                $scope.resetForm();
+                    }
+                }).then(function (response) {
+                    vm.data = response.data;
 
-            }, function (response) {
-                vm.data = response.data || 'Request failed';
-                $scope.msg_error = "Erro ao adicionar o cartão";
-            });
-        };
-    }]);
+                    // Mensagem adicionada para aparecer na view como retorno para o usuário
+                    $scope.msg = "Cartão adicionado com sucesso!";
+
+                    $scope.resetForm();
+
+                }, function (response) {
+                    vm.data = response.data || 'Request failed';
+
+                    // Mensagem adicionada para aparecer na view como retorno para o usuário
+                    $scope.msg_error = "Erro ao adicionar o cartão";
+                });
+            };
+        }]);
