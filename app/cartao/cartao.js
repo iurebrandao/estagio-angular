a6c751d8ab538ec482ae4defd9e55073211524a8
iurebrandao <iurebrandao@gmail.com>
2017-08-15 18:19:02 -0300

    Fazendo modal com proprio controller

--- app/cartao/cartao.js
+++ app/cartao/cartao.js
@@ -2,9 +2,7 @@
 
 angular.module('myApp.cartao', ['ngRoute'])
 
-
-
-    .controller('CartaoCtrl', ["$scope","$http", "config", function ($scope, $http, config) {
+    .controller('CartaoCtrl', ["$scope","$http", "config", "$uibModal", "$log", "$document", function ($scope, $http, config, $uibModal, $log, $document) {
         var vm = this;
 
         // Inicializa a variável payments para receber os pagamentos de cada cartão
@@ -55,56 +53,6 @@ angular.module('myApp.cartao', ['ngRoute'])
             });
         };
 
-        // Função que altera as informações de um cartão a partir das informações novas que o
-        // usuário quer editar
-        vm.editCard = function (id_cartao,nome,numero,bandeira,mes,ano,limite) {
-
-            var mes_exp = parseInt(mes);
-            var ano_exp = parseInt(ano);
-
-            // Faz a requisição "PATCH" para editar um cartão
-            $http({
-                method: "PATCH",
-                url: config.URL + "cards/" + id_cartao,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + config.KEY
-                },
-                data: {
-                    "id": id_cartao,
-                    "name": nome,
-                    "number": numero,
-                    "brand": bandeira,
-                    "exp_month": mes_exp,
-                    "exp_year": ano_exp,
-                    "limit": limite
-                }
-            }).then(function (response) {
-
-                // Adiciona as informações de retorno da requisição na variável do controler
-                vm.data = response.data;
-
-                // Mensagem adicionada para aparecer na view como retorno para o usuário
-                $scope.msg_user = "Cartão editado com sucesso!";
-
-                // Chama a função para atualizar a lista de cartões com as novas informações
-                vm.getCards();
-
-
-            }, function (response)  {
-
-                // Adiciona as informações de retorno da requisição na variável do controler
-                vm.data = response.data || 'Request failed';
-
-                // Mensagem adicionada para aparecer na view como retorno para o usuário
-                $scope.msg_user_error = "Erro ao editar o cartão";
-
-                // Chama a função para atualizar a lista de cartões com as novas informações
-                vm.getCards();
-
-            });
-        };
-
         // Função que deleta um cartão da lista de cartões a partir do "id" desse cartão
         vm.deleteCard = function (id_cartao) {
 
@@ -135,6 +83,32 @@ angular.module('myApp.cartao', ['ngRoute'])
                 // Mensagem adicionada para aparecer na view como retorno para o usuário
                 $scope.msg_user_error = "Erro ao deletar o cartão";
 
+            });
+        };
+
+        vm.openModal = function (size, parentSelector, id_cartao) {
+
+            var parentElem = parentSelector ?
+                angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+            var modalInstance = $uibModal.open({
+                animation: vm.animationsEnabled,
+                templateUrl: 'cartao/modal_editar_cartao.html',
+                controller: 'ModalEditarCartaoCtrl',
+                controllerAs: 'vmModal',
+                size: size,
+                resolve: {
+                    id_cartao: function () {
+                        return id_cartao;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function () {
+                // Chama a função para atualizar a lista de cartões com as novas informações
+                vm.getCards();
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
             });
         };
 
